a296a84eaac05c649d968b0ff5bcca220b60fb65
Dilip Ray Ch <i@iraycd.com>
2025-03-24 00:09:30 +0530

    Upgraded local and corrected the opacity with value

--- lib/features/dashboard/screens/dashboard_screen.dart
+++ lib/features/dashboard/screens/dashboard_screen.dart
@@ -149,7 +149,9 @@ class DashboardScreen extends StatelessWidget {
                   Container(
                     padding: const EdgeInsets.all(12),
                     decoration: BoxDecoration(
-                      color: color.withOpacity(0.2),
+                      color: color.withValues(
+                        alpha: 0.2,
+                      ),
                       borderRadius: BorderRadius.circular(12),
                     ),
                     child: Icon(
